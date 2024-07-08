4351546a3c54fb2c78f579dec1310e0bc1fafdad
dfreeman3102 <davidfreeman3102@gmail.com>
2024-07-08 19:36:48 -0400

    added routerProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -64,7 +64,5 @@ const router = createBrowserRouter([
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
