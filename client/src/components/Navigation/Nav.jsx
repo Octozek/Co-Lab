e835127497c9cb09741541780bd6095b83699868
dfreeman3102 <davidfreeman3102@gmail.com>
2024-07-09 19:36:30 -0400

    added imgs for each nav button

--- client/src/components/Navigation/Nav.jsx
+++ client/src/components/Navigation/Nav.jsx
@@ -1,25 +1,43 @@
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 export default function Nav() {
-    return (
-        <nav>
-            <div>
-            <Link to="/coming-events">Upcoming Events</Link>
-            </div>
-            <div>
-            <Link to="/past-events">Past Events</Link>
-            </div>
-            <div>
-            <Link to="/lessons">Lessons</Link>
-            </div>
-            <div>
-            <Link to="/chat">Chat</Link>
-            </div>
-            <div>
-            <Link to="/about-leader">Leaders</Link>
-            </div>
-            <div>
-            <Link to="/games">Games</Link>
-            </div>
-        </nav>
-    )
+  return (
+    <nav>
+      <div className="nav-link">
+        <Link to="/coming-events">
+          <img
+            src="../../../public/imgs/upcoming-events.png"
+            alt="upcoming events"
+          />
+        </Link>
+      </div>
+      <div className="nav-link">
+        <Link to="/past-events">
+          <img
+            src="../../../public/imgs/previous-events.png"
+            alt="previous events"
+          />
+        </Link>
+      </div>
+      <div className="nav-link">
+        <Link to="/lessons">
+          <img src="../../../public/imgs/lessons.png" alt="lessons" />
+        </Link>
+      </div>
+      <div className="nav-link">
+        <Link to="/chat">
+          <img src="../../../public/imgs/chat.png" alt="chat" />
+        </Link>
+      </div>
+      <div className="nav-link">
+        <Link to="/about-leader">
+          <img src="../../../public/imgs/leaders.png" alt="about leader" />
+        </Link>
+      </div>
+      <div className="nav-link">
+        <Link to="/games">
+          <img src="../../../public/imgs/games.png" alt="games" />
+        </Link>
+      </div>
+    </nav>
+  );
 }
