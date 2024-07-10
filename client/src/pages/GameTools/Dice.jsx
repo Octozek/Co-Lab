779f4f740a2f5ee05ef02a844a6edea618b4291d
dfreeman3102 <davidfreeman3102@gmail.com>
2024-07-09 21:28:25 -0400

    added basic dice game, will add img changing later

--- client/src/pages/GameTools/Dice.jsx
+++ client/src/pages/GameTools/Dice.jsx
@@ -1,8 +1,25 @@
-export default function Dice() {
-  
-    return (
-        <div>
-        <h1>Pair of Dice</h1>
-        </div>
-    );
-    }
+// src/RollDice.jsx
+import { useState } from 'react';
+
+const RollDice = () => {
+  const [diceNumbers, setDiceNumbers] = useState([1, 1]);
+
+  const rollDice = () => {
+    const newNumbers = [
+      Math.floor(Math.random() * 6) + 1,
+      Math.floor(Math.random() * 6) + 1,
+    ];
+    setDiceNumbers(newNumbers);
+  };
+
+  return (
+    <div className="roll-dice">
+        <h1>Roll Dice</h1>
+        <p>{diceNumbers[0]}</p>
+        <p>{diceNumbers[1]}</p>
+      <button onClick={rollDice}>Roll Dice</button>
+    </div>
+  );
+};
+
+export default RollDice;
