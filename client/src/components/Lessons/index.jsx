01f91727667f87821715c7261acb0cd6c3a2e5aa
baileyellison <beeusc2018@gmail.com>
2024-07-09 21:35:28 -0400

    added children to to lesson..we will see how this plays out

---
+++ client/src/components/Lessons/index.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+export default function CreateLesson() {
+  const lessons = [
+    {
+      title: "Create a Lesson",
+      image: "./public/",
+      description: "Create a new lesson here!",
+      path: "/createLesson/createLessonbtn",
+    },
+  ];
+
+  return (
+    <div>
+      {lessons.map((lesson, index) => (
+        <div key={index}>
+          <h3>{lesson.title}</h3>
+          <Link to={lesson.path}>
+            <img src={lesson.image} alt={lesson.title} />
+          </Link>
+          <p>{lesson.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
