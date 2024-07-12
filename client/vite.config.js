2fb51d2bbf6d6a8db9a1fc0b66d194d7f3fecf73
Octozek <octozek@gmail.com>
2024-07-11 22:33:39 -0500

    Ensure @vitejs/plugin-react is included and configured

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    server: {
+  server: {
     port: 3000,
     open: true,
     proxy: {
@@ -15,4 +15,4 @@ export default defineConfig({
       },
     }
   }
-})
+});
