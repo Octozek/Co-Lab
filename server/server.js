3bd3f7d7ae0dc2457d84a8bb26eb254cb8a7f9d1
Octozek <octozek@gmail.com>
2024-07-12 21:23:26 -0500

    Update MongoDB connection and environment variables

--- server/server.js
+++ server/server.js
@@ -10,9 +10,15 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/co-lab', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((err) => {
+  console.error('Error connecting to MongoDB:', err.message);
 });
 
 app.use('/api', eventRoutes);
