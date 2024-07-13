040468aaec25a5e8ceaa9d4272d2db4b44c45745
Octozek <octozek@gmail.com>
2024-07-12 20:46:23 -0500

    Prepare project for deployment on Render

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,8 +11,8 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:3001',
         changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+        secure: false
+      }
+    }
+  }
 });
