79c109ecc0d8b96163b9850fbe8eb20d60a145c2
Jinkc21 <jin.k.choe@gmail.com>
2024-07-13 18:31:24 -0400

    updated vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: path.resolve(__dirname, 'dist'),
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/graphql': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false
+      }
+    }
   }
 });
