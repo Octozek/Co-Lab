5c8cb0cf9134776ab2cea9c7eac05013ff69932c
Octozek <octozek@gmail.com>
2024-07-13 17:22:31 -0500

    Add postinstall script to handle build process for Render

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/graphql': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false
-      }
-    }
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
   }
 });
