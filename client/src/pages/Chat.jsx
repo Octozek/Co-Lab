e4e2002ebbb96c1c72a9b2cf024f053d8b0c4dc5
Jinkc21 <jin.k.choe@gmail.com>
2024-07-15 15:33:17 -0400

    successfully removed ChatForm for Guardians

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,30 +1,31 @@
-import { useQuery } from '@apollo/client';
+import { useQuery } from "@apollo/client";
 // import Auth from '../utils/auth';
-import React, { useEffect, useState } from 'react';
-import ChatList from '../components/ChatList';
-import ChatForm from '../components/ChatForm';
+import React, { useEffect, useState } from "react";
+import ChatList from "../components/ChatList";
+import ChatForm from "../components/ChatForm";
 
-import { QUERY_CHATS, QUERY_USERS } from '../utils/queries';
+import { QUERY_CHATS, QUERY_USERS, QUERY_ME } from "../utils/queries";
 
 const Chat = () => {
-
   // const { userLoading, userData } = useQuery(QUERY_ME);
   // const currentUser = userData?.me || {};
   // console.log("currentUser", userData)
-  
+
   // const { loading, data } = useQuery(QUERY_CHATS);
   // const chats = data?.getChats || [];
   // console.log("chats", chats)
 
   const { loading: chatsLoading, data: chatsData } = useQuery(QUERY_CHATS);
   const { loading: usersLoading, data: usersData } = useQuery(QUERY_USERS);
+  const { loading: userLoading, data: userData } = useQuery(QUERY_ME);
   const chats = chatsData?.getChats || [];
   const users = usersData?.getUsers || [];
+  const currentUser = userData?.me || {};
+  console.log("users", users);
 
   return (
     <main>
-     
-       <div className="flex-row justify-center">
+      <div className="flex-row justify-center">
         <div id="userListContainer" className="col-12 col-md-6 mb-3">
           <h3>Chat Members</h3>
           {usersLoading ? (
@@ -36,20 +37,22 @@ const Chat = () => {
               ))}
             </ul>
           )}
-     
         </div>
-        <div id="chatFormContainer" className="col-12 col-md-6 mb-3" style={{ border: '1px dotted #1a1a1a' }}>
-          <ChatForm />
-        </div>
-     
+        {currentUser.role !== "Guardian" && (
+          <div
+            id="chatFormContainer"
+            className="col-12 col-md-6 mb-3"
+            style={{ border: "1px dotted #1a1a1a" }}
+          >
+            <ChatForm />
+          </div>
+        )}
+
         <div id="chatListContainer" className="col-12 col-md-8 mb-3">
           {chatsLoading ? (
             <div>Loading...</div>
           ) : (
-            <ChatList
-              chats={chats}
-              title="Current Chat(s)..."
-            />
+            <ChatList chats={chats} title="Current Chat(s)..." />
           )}
         </div>
       </div>
