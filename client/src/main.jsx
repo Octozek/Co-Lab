d1cecd5375546fd33008bfd7384486c3012ea574
Jinkc21 <jin.k.choe@gmail.com>
2024-07-15 18:16:00 -0400

    formatted

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,92 +1,91 @@
-import ReactDOM from 'react-dom/client'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import App from './App.jsx'
-import './index.css'
+import App from "./App.jsx";
+import "./index.css";
 
-import Home from './pages/Home.jsx'
-import Error from './pages/Error.jsx'
-import AboutLeader from './pages/AboutLeader.jsx'
-import Chat from './pages/Chat.jsx'
-import ComingEvents from './pages/ComingEvents.jsx'
-import Games from './pages/Games.jsx'
-import Lessons from './pages/Lessons.jsx'
-import Login from './pages/Login.jsx'
-import Signup from './pages/Signup';
-import PastEvents from './pages/PastEvents.jsx'
-import Settings from './pages/Settings.jsx'
-import Dice from './pages/GameTools/Dice.jsx'
-import Timer from './pages/GameTools/Timer.jsx'
-import Scoreboard from './pages/GameTools/Scoreboard.jsx'
-import SingleChat from './pages/SingleChat.jsx'
+import Home from "./pages/Home.jsx";
+import Error from "./pages/Error.jsx";
+import AboutLeader from "./pages/AboutLeader.jsx";
+import Chat from "./pages/Chat.jsx";
+import ComingEvents from "./pages/ComingEvents.jsx";
+import Games from "./pages/Games.jsx";
+import Lessons from "./pages/Lessons.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup";
+import PastEvents from "./pages/PastEvents.jsx";
+import Settings from "./pages/Settings.jsx";
+import Dice from "./pages/GameTools/Dice.jsx";
+import Timer from "./pages/GameTools/Timer.jsx";
+import Scoreboard from "./pages/GameTools/Scoreboard.jsx";
+import SingleChat from "./pages/SingleChat.jsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     errorElement: <Error />,
     children: [
       {
-        index : true,
+        index: true,
         element: <Home />,
       },
       {
-        path: '/about-leader',
+        path: "/about-leader",
         element: <AboutLeader />,
       },
       {
-        path: '/chat',
-        element: 
-           <Chat />
+        path: "/chat",
+        element: <Chat />,
       },
       {
-        path: '/coming-events',
+        path: "/coming-events",
         element: <ComingEvents />,
       },
       {
-        path: '/games',
+        path: "/games",
         element: <Games />,
       },
       {
-        path: '/games/dice',
+        path: "/games/dice",
         element: <Dice />,
       },
       {
-        path: '/games/timer',
+        path: "/games/timer",
         element: <Timer />,
       },
       {
-        path: '/games/scoreboard',
+        path: "/games/scoreboard",
         element: <Scoreboard />,
-      },      
+      },
       {
-        path: '/lessons',
+        path: "/lessons",
         element: <Lessons />,
       },
       {
-        path: '/login',
+        path: "/login",
         element: <Login />,
       },
       {
-        path: '/signup',
-        element: <Signup />
+        path: "/signup",
+        element: <Signup />,
       },
       {
-        path: '/past-events',
+        path: "/past-events",
         element: <PastEvents />,
       },
       {
-        path: '/settings',
+        path: "/settings",
         element: <Settings />,
       },
       {
-        path: '/chats/:chatId',
+        path: "/chats/:chatId",
         element: <SingleChat />,
-      }
-    ]
-  }
-])
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
-)
+);
