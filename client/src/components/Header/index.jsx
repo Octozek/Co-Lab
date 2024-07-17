541402828e7e46fd26d95da1cce7acecc598d44a
Jinkc21 <jin.k.choe@gmail.com>
2024-07-17 01:09:28 -0400

    added profilePic to logged in authentication

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -8,43 +8,48 @@ const Header = () => {
   };
 
   return (
-    <header className="header">
-      <div className="profilePic">
-        <img src="https://via.placeholder.com/150" alt="Group Logo" />
-      </div>
-      <h1 className="groupTitle">Co-Lab</h1>
-      <div className="rightsideHeader">
-        <div className="authButtons">
-          {Auth.loggedIn() ? (
-            <div className="homeAndsettings">
-              <div className="home-icon">
-                <Link to="/">
-                  <img src="./imgs/Home.png" alt="home" />
+      <header className="header">
+            {Auth.loggedIn() ? (
+        
+              <>
+                <div className="profilePic">
+                  <img src="https://via.placeholder.com/150" alt="Group Logo" />
+                </div>
+                <h1 className="groupTitle">Co-Lab</h1>
+                <div className="rightsideHeader">
+                  <div className="authButtons">
+                    <div className="homeAndsettings">
+                      <div className="home-icon">
+                        <Link to="/">
+                          <img src="./imgs/Home.png" alt="home" />
+                        </Link>
+                      </div>
+                      <div className="settings-icon">
+                        <Link to="/settings">
+                          <img src="./imgs/settings.png" alt="settings" />
+                        </Link>
+                      </div>
+                      <button className="btn btn-lg btn-light m-2" onClick={logout}>
+                        Logout
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </>
+   
+            ) : (
+              <>
+              <h1 className="groupTitle">Co-Lab</h1>
+                <Link className="btn btn-lg btn-info m-2" to="/login">
+                  Login
                 </Link>
-              </div>
-              <div className="settings-icon">
-                <Link to="/settings">
-                  <img src="./imgs/settings.png" alt="settings" />
+                <Link className="btn btn-lg btn-light m-2" to="/signup">
+                  Signup
                 </Link>
-              </div>
-              <button className="btn btn-lg btn-light m-2" onClick={logout}>
-                Logout
-              </button>
-            </div>
-          ) : (
-            <>
-              <Link className="btn btn-lg btn-info m-2" to="/login">
-                Login
-              </Link>
-              <Link className="btn btn-lg btn-light m-2" to="/signup">
-                Signup
-              </Link>
-            </>
-          )}
-        </div>
-      </div>
-    </header>
-  );
+              </>
+            )}
+      </header>
+    );
 };
 
 export default Header;
